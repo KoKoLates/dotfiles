ae93217ee116f4af8bfb29db6416810abd8c5b67
KoKoLates <the21515@gmail.com>
2023-07-26 18:04:21 +0800

    feat(powershell): add PSReadLine

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,9 +1,24 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
 # Prompt
 Import-Module posh-git
-oh-my-posh --init --shell pwsh | Invoke-Expression
+$posh_config = Join-Path $PSScriptRoot ".\koko.omp.json"
+oh-my-posh --init --shell pwsh --config $posh_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Alias
 Set-Alias vim nvim
 Set-Alias ll ls
 Set-Alias grep findstr
 
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue | 
+  Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
